47c01c13e78b1f2e07997d333c5ce4319614f123
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-05-22 23:43:00 +0200

    admin: add login

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,9 +1,88 @@
-import { ReactElement } from "react";
 import AdminLayout from "../../layout/AdminLayout";
+import type { FormEvent, ReactElement } from "react";
 import type NextPageLayout from "../../types/NextPageLayout";
+import { useState } from "react";
+import axios from "axios";
+
+interface IUserCredential {
+  username: string;
+  password: string;
+}
+
+interface ILoginResp {
+  data: {
+    token: string;
+    result: boolean;
+  }
+}
 
 const AdminIndex: NextPageLayout = (): ReactElement => {
-  return <div>admin page</div>;
+  const [userData, setUserData] = useState<IUserCredential>({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: FormEvent<HTMLInputElement>): void => {
+    setUserData({
+      ...userData,
+      [e.currentTarget.name]: e.currentTarget.value.trim(),
+    });
+  };
+
+  const handleSubmit = async (e: React.SyntheticEvent): Promise<void> => {
+    e.preventDefault();
+
+    const { username, password } = userData;
+    const resp: ILoginResp = await axios({
+      method: "POST",
+      url: "http://localhost:5010/login",
+      data: { username: username, password: password },
+    });
+
+    const result: boolean = resp.data.result;
+    const token: string = resp.data.token;
+    console.log("Result: ", result);
+    console.log("Token: ", token);
+  };
+
+  return (
+    <>
+      <div className="md:w-4/6 lg:w-3/6 2xl:w-1/3 mx-auto mt-10">
+        <form onSubmit={handleSubmit}>
+          <div className="w-4/6 mx-auto my-4">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="username"
+              id="email"
+              className="block mt-2 w-full border border-gray-600 p-3 focus:ring-0 rounded-lg shadow-sm focus:shadow-md"
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="w-4/6 mx-auto my-4">
+            <label htmlFor="name">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="block mt-2 w-full border border-gray-600 p-3 focus:ring-0 rounded-lg shadow-sm focus:shadow-md"
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="block mx-auto w-3/6 border p-5 bg-white my-10 hover:shadow-md focus:bg-gray-200 focus:shadow-md"
+          >
+            login
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 AdminIndex.getLayout = (page: ReactElement) => (
