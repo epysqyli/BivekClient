5b5b08610367f0542f75e4cd135fe8a2cb803355
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-06-04 15:12:38 +0200

    tag: add TagRepo

---
+++ lib/TagRepo.ts
@@ -0,0 +1,46 @@
+import axios, { AxiosResponse } from "axios";
+import type { Tag } from "../interfaces/IArticle";
+
+const getTags = async (): Promise<AxiosResponse<Array<Tag>>> => {
+  return await axios({
+    method: "GET",
+    url: "http://localhost:5010/tags"
+  });
+};
+
+const getTagById = async (id: number): Promise<AxiosResponse<Tag>> => {
+  return await axios({
+    method: "GET",
+    url: `http://localhost:5010/tags/${id}`
+  });
+};
+
+const createTag = async (name: string): Promise<AxiosResponse<Tag>> => {
+  return await axios({
+    method: "POST",
+    url: "http://localhost:5010/tags",
+    data: { name },
+    withCredentials: true
+  });
+};
+
+const patchTag = async (id: number, path: string, op: string, value: string): Promise<AxiosResponse<Tag>> => {
+  const patchData = [{ path: path, op: op, value: value }];
+
+  return await axios({
+    method: "PATCH",
+    url: `http://localhost:5010/tags/${id}`,
+    data: patchData,
+    withCredentials: true
+  });
+};
+
+const deleteTag = async (id: number) => {
+  return await axios({
+    method: "DELETE",
+    url: `http://localhost:5010/tags/${id}`,
+    withCredentials: true
+  });
+};
+
+export { getTags, getTagById, createTag, patchTag, deleteTag };
