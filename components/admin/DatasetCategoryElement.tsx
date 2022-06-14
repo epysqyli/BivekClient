b6e27896cec368c15534b5e2de0d17c0993d7f35
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-06-14 23:22:34 +0200

    datasets: setup admin pape

--- components/admin/DatasetCategoryElement.tsx
+++ components/admin/DatasetCategoryElement.tsx
@@ -11,13 +11,17 @@ const DatasetCategoryElement = ({ dataCategory }: Props): ReactElement => {
     <>
       <h3 className='my-5 text-center text-xl font-bold'>{dataCategory.name}</h3>
       <div className='my-5'>
-        {dataCategory.datasets.map((dataset) => {
-          return (
-            <div className='w-5/6 border mx-auto px-2 py-2' key={dataset.id}>
-              <DatasetElement dataset={dataset} />
-            </div>
-          );
-        })}
+        {dataCategory.datasets !== null && dataCategory.datasets.length !== 0 ? (
+          dataCategory.datasets.map((dataset) => {
+            return (
+              <div className='w-5/6 border mx-auto px-2 py-2' key={dataset.id}>
+                <DatasetElement dataset={dataset} />
+              </div>
+            );
+          })
+        ) : (
+          <div className='w-5/6 border mx-auto px-2 py-2 text-center'>This category is still empty</div>
+        )}
       </div>
     </>
   );
--- pages/admin/manage-datasets.tsx
+++ pages/admin/manage-datasets.tsx
@@ -1,12 +1,14 @@
-import type { ReactElement } from "react";
+import type { FormEvent, ReactElement } from "react";
 import type NextPageLayout from "../../types/NextPageLayout";
 import type { GetServerSideProps, Redirect, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+import { useState } from "react";
 import AdminLayout from "../../layouts/AdminLayout";
 import { checkLogin } from "../../lib/Auth";
 import TopElement from "../../components/admin/TopElement";
-import { getDataCategories } from "../../lib/DataCategoryRepo";
+import { createDataCategory, getDataCategories } from "../../lib/DataCategoryRepo";
 import IDataCategory from "../../interfaces/models/IDataCategory";
 import DatasetCategoryElement from "../../components/admin/DatasetCategoryElement";
+import { PlusCircle } from "react-feather";
 
 export const getServerSideProps: GetServerSideProps<{} | Redirect> = async (
   context: GetServerSidePropsContext
@@ -21,22 +23,48 @@ export const getServerSideProps: GetServerSideProps<{} | Redirect> = async (
     };
   }
 
-  const datasets = await getDataCategories();
-  return { props: { datasets: datasets.data } };
+  const datasetCategories = await getDataCategories();
+  return { props: { datasetCategoriesProps: datasetCategories.data } };
 };
 
 interface Props {
-  datasets: Array<IDataCategory>;
+  datasetCategoriesProps: Array<IDataCategory>;
 }
 
-const DatasetCategories: NextPageLayout<Props> = ({ datasets }: Props): ReactElement => {
+const DatasetCategories: NextPageLayout<Props> = ({ datasetCategoriesProps }: Props): ReactElement => {
+  const [categoryName, setCategoryName] = useState<string>("");
+  const [datasetCategories, setDatasetCategories] = useState<Array<IDataCategory>>(datasetCategoriesProps);
+  const handleCreateCategory = async (): Promise<void> => {
+    const newDataCategory = await createDataCategory(categoryName);
+    setDatasetCategories([...datasetCategories, newDataCategory.data]);
+  };
+  const handleCategoryChange = (e: FormEvent<HTMLInputElement>) => setCategoryName(e.currentTarget.value);
+
   return (
     <>
       <TopElement text='Manage datasets and data categories' />
+      <div className='flex items-end text-lg w-5/6 mx-auto rounded-md mt-10 mb-16'>
+        <div className='w-min mx-auto cursor-pointer group'>
+          <PlusCircle
+            size={42}
+            strokeWidth={1.25}
+            fill='gray'
+            color='white'
+            className='border-gray-400 border-2 rounded-full group-hover:scale-95 group-active:scale-75 transition-transform'
+            onClick={handleCreateCategory}
+          />
+        </div>
+        <input
+          type='text'
+          className='border-b-2 border-gray-300 block w-4/5 mx-auto py-2 pl-3 text-center focus:outline-none'
+          placeholder='Enter data category name'
+          onChange={handleCategoryChange}
+        />
+      </div>
       <div>
-        {datasets.map((dataCategory) => {
+        {datasetCategories.map((dataCategory) => {
           return (
-            <div className='w-11/12 mx-auto border py-2 shadow-md rounded' key={dataCategory.id}>
+            <div className='w-11/12 mx-auto border py-2 my-10 shadow-md rounded' key={dataCategory.id}>
               <DatasetCategoryElement dataCategory={dataCategory} />
             </div>
           );
