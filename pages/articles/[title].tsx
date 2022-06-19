16230113919803ad72746b7d5d8e004f1742f662
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-06-19 21:37:00 +0200

    article: change title style in show

--- pages/articles/[title].tsx
+++ pages/articles/[title].tsx
@@ -43,7 +43,7 @@ export const getServerSideProps: GetServerSideProps = async (context: GetServerS
 const Article: NextPageLayout<Props> = ({ article, body }: Props): ReactElement => {
   return (
     <>
-      <h1 className='text-2xl text-center my-5'>{article.title}</h1>
+      <h1 className='text-4xl font-semibold text-gray-700 text-center mt-8 mb-10'>{article.title}</h1>
       <div className='w-11/12 md:w-4/5 lg:w-1/2 mx-auto'>
         <div className='ProseMirror' dangerouslySetInnerHTML={{ __html: body }}></div>
       </div>
