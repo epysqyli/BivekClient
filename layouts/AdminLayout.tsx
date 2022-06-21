e25ef92c84888bf20f323c57c90a5f09a6dcd5a5
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-06-21 16:44:55 +0200

    admin: add footer links

--- layouts/AdminLayout.tsx
+++ layouts/AdminLayout.tsx
@@ -2,6 +2,8 @@ import type ILayoutProps from "../interfaces/ILayoutProps";
 import type { ReactElement } from "react";
 import { useState } from "react";
 import { OverlayProvider } from "../hooks/OverlayContext";
+import Link from "next/link";
+import { Globe, Settings } from "react-feather";
 
 const AdminLayout = ({ children }: ILayoutProps): ReactElement => {
   const [activeOverlay, setActiveOverlay] = useState(false);
@@ -11,9 +13,24 @@ const AdminLayout = ({ children }: ILayoutProps): ReactElement => {
 
   return (
     <OverlayProvider value={{ showOverlay, hideOverlay }}>
-      <header className='bg-orange-200 text-center'>ADMIN LAYOUT HEADER</header>
-      <main>{children}</main>
-      <footer className='bg-orange-200 text-center'>ADMIN LAYOUT FOOTER</footer>
+      <header></header>
+      <main className='pb-10'>{children}</main>
+      <footer className='fixed bottom-0 w-full bg-slate-100 text-center py-3'>
+        <div className='flex justify-around items-center w-4/5 mx-auto'>
+          <Link href='/'>
+            <Globe
+              strokeWidth={1.5}
+              className='cursor-pointer text-slate-500 hover:text-slate-700 transition-transform hover:scale-95 active:scale-90'
+            />
+          </Link>
+          <Link href='/admin'>
+            <Settings
+              strokeWidth={1.5}
+              className='cursor-pointer text-slate-500 hover:text-slate-700 transition-transform hover:scale-95 active:scale-90'
+            />
+          </Link>
+        </div>
+      </footer>
       {activeOverlay ? <div className={overlay}></div> : null}
     </OverlayProvider>
   );
