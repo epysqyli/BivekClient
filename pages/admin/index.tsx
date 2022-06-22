8a068eeebf487bdd0f5d671135df8ae50f1ba877
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-06-22 17:01:26 +0200

    admin: make index responsive

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -30,7 +30,7 @@ const AdminIndex: NextPageLayout = (): ReactElement => {
   return (
     <>
       <TopElement text='Manage blog resources' />
-      <div className='my-10 w-5/6 mx-auto'>
+      <div className='my-10 mx-auto w-5/6 md:w-4/6 lg:w-2/5 xl:w-1/3'>
         <IndexLink
           text='Create a new article'
           href='/admin/create-new-article'
