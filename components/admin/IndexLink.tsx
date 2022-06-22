abca3dd8fa61b0a3578a47deda30a10e6bf02c60
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-06-22 16:57:17 +0200

    index link: fix text and div width

--- components/admin/IndexLink.tsx
+++ components/admin/IndexLink.tsx
@@ -12,7 +12,7 @@ const IndexLink = ({ text, href, icon }: Props): ReactElement => {
     <Link href={href}>
       <div className='flex justify-between items-center my-6 rounded shadow shadow-slate-300 bg-white transition-colors hover:bg-slate-500 cursor-pointer group'>
         <div className='flex items-center self-stretch px-4 bg-slate-400 rounded-tl rounded-bl'>{icon}</div>
-        <div className='text-lg text-gray-800 group-hover:text-white w-4/5 text-center py-5'>{text}</div>
+        <div className='text-lg text-gray-800 group-hover:text-white text-center w-full py-5'>{text}</div>
       </div>
     </Link>
   );
