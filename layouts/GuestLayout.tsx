2c34a977dac64648654f20b53bd4bea6e2d5030f
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-06-22 20:52:25 +0200

    guest layout: add sidemenu button

--- layouts/GuestLayout.tsx
+++ layouts/GuestLayout.tsx
@@ -1,12 +1,19 @@
 import type { ReactElement } from "react";
+import { Menu } from "react-feather";
 import ILayoutProps from "../interfaces/ILayoutProps";
 
 const GuestLayout = ({ children }: ILayoutProps): ReactElement => {
   return (
     <>
-      <header className="bg-slate-300 text-center">HEADER</header>
-      <main>{children}</main>
-      <footer className="bg-slate-300 text-center">FOOTER</footer>
+      <header className='bg-slate-300 text-center'>HEADER</header>
+      <main className='relative'>
+        <div>{children}</div>
+        <Menu
+          size={40}
+          className='absolute bg-white bottom-5 right-5 p-2 rounded-full shadow-md shadow-slate-400 cursor-pointer transition-transform hover:scale-95 active:scale-90'
+        />
+      </main>
+      <footer className='bg-slate-300 text-center'>FOOTER</footer>
     </>
   );
 };
