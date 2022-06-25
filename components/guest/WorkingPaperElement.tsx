b0f5cc2ef644c58594ae363a970e67385cdcd50e
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-06-25 13:08:21 +0200

    guest: setup working papers

---
+++ components/guest/WorkingPaperElement.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { ReactElement } from "react";
+import { Download } from "react-feather";
+import IWorkingPaper from "../../interfaces/models/IWorkingPaper";
+
+interface Props {
+  workingPaper: IWorkingPaper;
+}
+
+const WorkingPaperElement = ({ workingPaper }: Props): ReactElement => {
+  return (
+    <div className='my-10 py-6 px-2 w-5/6 mx-auto border-b-2'>
+      <h2 className='text-2xl text-center mb-8 font-medium text-slate-700'>{workingPaper.title}</h2>
+      <div className='text-slate-700 w-5/6 mx-auto text-justify'>{workingPaper.abstract}</div>
+      <Link href={workingPaper.link}>
+        <Download
+          size={30}
+          strokeWidth={1.5}
+          className='w-fit mx-auto mt-6 text-slate-600 cursor-pointer transition-transform hover:scale-95 active:scale-90'
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default WorkingPaperElement;
--- pages/working-papers.tsx
+++ pages/working-papers.tsx
@@ -1,9 +1,31 @@
 import type { ReactElement } from "react";
 import type NextPageLayout from "../types/NextPageLayout";
+import type { GetServerSideProps } from "next";
+import type { AxiosResponse } from "axios";
 import GuestLayout from "../layouts/GuestLayout";
+import IWorkingPaper from "../interfaces/models/IWorkingPaper";
+import { getWorkingPapers } from "../lib/WorkingPaperRepo";
+import WorkingPaperElement from "../components/guest/WorkingPaperElement";
 
-const WorkingPapers: NextPageLayout = (): ReactElement => {
-  return <div></div>;
+export const getServerSideProps: GetServerSideProps = async () => {
+  const resp: AxiosResponse<Array<IWorkingPaper>> = await getWorkingPapers();
+  const workingPapers: Array<IWorkingPaper> = resp.data;
+
+  return { props: { workingPapers } };
+};
+
+interface Props {
+  workingPapers: Array<IWorkingPaper>;
+}
+
+const WorkingPapers: NextPageLayout<Props> = ({ workingPapers }: Props): ReactElement => {
+  return (
+    <>
+      {workingPapers.map((wp) => (
+        <WorkingPaperElement workingPaper={wp} key={wp.id} />
+      ))}
+    </>
+  );
 };
 
 WorkingPapers.getLayout = (page: ReactElement) => <GuestLayout>{page}</GuestLayout>;
