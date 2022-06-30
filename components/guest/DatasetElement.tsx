a868ca66c5e3993f3da9f869f250f1527d08d0f4
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-06-30 15:36:04 +0200

    datasets: allow download clicking on the whole row

--- components/guest/DatasetElement.tsx
+++ components/guest/DatasetElement.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 const DatasetElement = ({ dataset }: Props): ReactElement => {
   return (
-    <div className='w-11/12 mx-auto px-2 py-3 flex justify-between border-b hover:bg-slate-200 active:bg-slate-300 hover:rounded'>
-      <div className='w-4/5 text-slate-700'>{dataset.title}</div>
-      <Link href={dataset.link}>
-        <Download className='w-1/5 text-slate-600 cursor-pointer transition-transform hover:scale-95 active:scale-90' />
-      </Link>
-    </div>
+    <Link href={dataset.link}>
+      <div className='w-11/12 mx-auto px-2 py-3 flex justify-between border-b hover:bg-slate-200 active:bg-slate-300 hover:rounded cursor-pointer'>
+        <div className='w-4/5 text-slate-700'>{dataset.title}</div>
+        <Download className='w-1/5 text-slate-600 transition-transform hover:scale-95 active:scale-90' />
+      </div>
+    </Link>
   );
 };
 
