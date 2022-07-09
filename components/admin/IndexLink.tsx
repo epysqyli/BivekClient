cf5647c7c88fffc19fb26d46c92d6d0a76179571
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-09 17:18:14 +0200

    admin: improve index link style

--- components/admin/IndexLink.tsx
+++ components/admin/IndexLink.tsx
@@ -10,9 +10,9 @@ interface Props {
 const IndexLink = ({ text, href, icon }: Props): ReactElement => {
   return (
     <Link href={href}>
-      <div className='flex justify-between items-center my-6 border-b-2 border-slate-300 hover:border-slate-50 transition-colors hover:bg-slate-500 cursor-pointer group'>
-        <div className='flex items-center self-stretch px-4 rounded-tl rounded-bl'>{icon}</div>
-        <div className='text-lg text-slate-800 group-hover:text-white text-center w-full py-5'>{text}</div>
+      <div className='py-7 border bg-slate-50 shadow-md shadow-slate-300 rounded-md hover:border-slate-50 hover:bg-slate-500 cursor-pointer hover:scale-105 transition-all group'>
+        <div className="mb-2">{icon}</div>
+        <div className='text-lg tracking-tight text-slate-800 group-hover:text-white text-center w-full'>{text}</div>
       </div>
     </Link>
   );
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -25,40 +25,40 @@ export const getServerSideProps: GetServerSideProps<{} | Redirect> = async (
 };
 
 const AdminIndex: NextPageLayout = (): ReactElement => {
-  const iconStyle: string = "text-slate-600 group-hover:text-slate-50 group-hover:scale-95 group-active:scale-75 transition-transform";
+  const iconStyle: string = "text-amber-700 group-hover:text-slate-50 group-hover:scale-95 group-active:scale-75 transition-transform mx-auto";
 
   return (
     <>
       <TopElement text='Manage blog resources' />
-      <div className='my-10 mx-auto w-5/6 md:w-4/6 lg:w-2/5 xl:w-1/3'>
+      <div className='my-10 mx-auto w-4/6 lg:w-3/5 xl:w-1/2 grid md:grid-cols-2 gap-y-5 md:gap-5'>
         <IndexLink
           text='Create a new article'
           href='/admin/create-new-article'
-          icon={<FilePlus size={30} strokeWidth={1.5} className={iconStyle} />}
+          icon={<FilePlus size={32} strokeWidth={1.5} className={iconStyle} />}
         />
 
         <IndexLink
           href='/admin/edit-articles'
           text='Manage existing articles'
-          icon={<Edit3 size={30} strokeWidth={1.5} className={iconStyle} />}
+          icon={<Edit3 size={32} strokeWidth={1.5} className={iconStyle} />}
         />
 
         <IndexLink
           href='/admin/article-tags'
           text='Article tags'
-          icon={<Tag size={30} strokeWidth={1.5} className={iconStyle} />}
+          icon={<Tag size={32} strokeWidth={1.5} className={iconStyle} />}
         />
 
         <IndexLink
           href='/admin/manage-datasets'
           text='Datasets'
-          icon={<Database size={30} strokeWidth={1.5} className={iconStyle} />}
+          icon={<Database size={32} strokeWidth={1.5} className={iconStyle} />}
         />
 
         <IndexLink
           href='/admin/manage-research-papers'
           text='Research papers'
-          icon={<Paperclip size={30} strokeWidth={1.5} className={iconStyle} />}
+          icon={<Paperclip size={32} strokeWidth={1.5} className={iconStyle} />}
         />
       </div>
     </>
