58ae4da6cd01c3ff46a224ac5de6f1856e46f112
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-20 21:42:39 +0200

    index: increase navbar icon and text size

--- components/guest/IndexLink.tsx
+++ components/guest/IndexLink.tsx
@@ -12,7 +12,7 @@ const IndexLink = ({ pageLink, item, icon }: Props): ReactElement => {
     <Link href={pageLink}>
       <div className='p-3 cursor-pointer border-b-2 border-transparent hover:border-b-2 hover:border-slate-300 active:bg-slate-100 active:text-slate-500 transition-colors group'>
         {icon}
-        <span className='block text mt-2'>{item}</span>
+        <span className='block text mt-2 lg:mt-4'>{item}</span>
       </div>
     </Link>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,27 +16,29 @@ const Home: NextPageLayout = (): ReactElement => {
       <div className='lg:my-10'>
         <SectionHeader resource='homepage' text='Blog title' />
       </div>
-      <nav className='py-3 lg:py-5 bg-slate-500 text-slate-50'>
+      <nav className='py-5 bg-slate-500 text-slate-50 lg:text-xl'>
         <div className='flex items-center justify-around flex-wrap mx-auto gap-x-2 w-11/12 lg:w-2/3'>
           <IndexLink
             pageLink='/articles'
             item='articles'
-            icon={<Type className='text-slate-100 group-active:text-slate-500 w-min mx-auto' />}
+            icon={<Type size={32} className='text-slate-100 group-active:text-slate-500 w-min mx-auto' />}
           />
           <IndexLink
             pageLink='/datasets'
             item='datasets'
-            icon={<Database className='text-slate-100 group-active:text-slate-500 w-min mx-auto' />}
+            icon={<Database size={32} className='text-slate-100 group-active:text-slate-500 w-min mx-auto' />}
           />
           <IndexLink
             pageLink='/research-papers'
             item='papers'
-            icon={<Paperclip className='text-slate-100 group-active:text-slate-500 w-min mx-auto' />}
+            icon={
+              <Paperclip size={32} className='text-slate-100 group-active:text-slate-500 w-min mx-auto' />
+            }
           />
           <IndexLink
             pageLink='/about'
             item='About'
-            icon={<Info className='text-slate-100 group-active:text-slate-500 w-min mx-auto' />}
+            icon={<Info size={32} className='text-slate-100 group-active:text-slate-500 w-min mx-auto' />}
           />
         </div>
       </nav>
