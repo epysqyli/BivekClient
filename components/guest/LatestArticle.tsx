76ab4b72faf642a4830d2e3d3e2ef530d779a883
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-22 22:28:14 +0200

    index: add small style changes

--- components/guest/LatestArticle.tsx
+++ components/guest/LatestArticle.tsx
@@ -13,7 +13,7 @@ interface Props extends ArticleLinkProps {
 const LatestArticle = ({ ArticleLink, article }: Props): ReactElement => {
   return (
     <>
-      <div className='flex items-center justify-around md:justify-center md:gap-x-32'>
+      <div className='flex items-center justify-around md:justify-center md:gap-x-32 mb-10'>
         <Activity className='text-amber-600 w-min animate-pulse' size={30} />
         <h2 className='text-3xl text-center text-slate-600 underline underline-offset-4 font-medium'>
           Latest article
