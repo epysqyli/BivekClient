c5d844669fd6e9e0fa9d591bb8c9b7d6a4398571
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-22 22:45:45 +0200

    latest articles: simplify layout

--- components/guest/LatestArticle.tsx
+++ components/guest/LatestArticle.tsx
@@ -13,13 +13,7 @@ interface Props extends ArticleLinkProps {
 const LatestArticle = ({ ArticleLink, article }: Props): ReactElement => {
   return (
     <>
-      <div className='flex items-center justify-around md:justify-center md:gap-x-32 mb-10'>
-        <Activity className='text-amber-600 w-min animate-pulse' size={30} />
-        <h2 className='text-3xl text-center text-slate-600 underline underline-offset-4 font-medium'>
-          Latest article
-        </h2>
-        <Activity className='text-amber-600 w-min animate-pulse' size={30} />
-      </div>
+      <Activity className='text-amber-600 w-min mx-auto animate-pulse' size={40} />
       <ArticleLink article={article} />
     </>
   );
