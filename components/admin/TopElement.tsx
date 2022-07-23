306c78cc3c8716d97ba1aad5d423a0565cad2763
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-23 10:52:36 +0200

    manage datasets: add error handling

--- components/admin/TopElement.tsx
+++ components/admin/TopElement.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react";
 
 const TopElement = ({ text }: { text: string }): ReactElement => {
   return (
-    <h1 className='w-5/6 mx-auto py-7 mt-2 mb-5 text-center text-4xl text-gray-700 font-bold'>{text}</h1>
+    <h1 className='mx-auto py-7 mt-2 mb-5 text-center text-4xl text-gray-700 font-bold'>{text}</h1>
   );
 };
 
--- pages/admin/manage-datasets.tsx
+++ pages/admin/manage-datasets.tsx
@@ -11,6 +11,8 @@ import DatasetCategoryElement from "../../components/admin/DatasetCategoryElemen
 import { PlusCircle } from "react-feather";
 import DeleteConfirmation from "../../components/admin/DeleteConfirmation";
 import { OverlayContext } from "../../hooks/OverlayContext";
+import { AxiosError } from "axios";
+import IValidationError from "../../interfaces/IValidationError";
 
 export const getServerSideProps: GetServerSideProps<{} | Redirect> = async (
   context: GetServerSidePropsContext
@@ -38,6 +40,7 @@ const DatasetCategories: NextPageLayout<Props> = ({ datasetCategoriesProps }: Pr
   const [datasetCategories, setDatasetCategories] = useState<Array<IDataCategory>>(datasetCategoriesProps);
   const [currentDataCategoryId, setCurrentDataCategoryId] = useState<number>(0);
   const [showDelete, setShowDelete] = useState<boolean>(false);
+  const [creationError, setCreationError] = useState<string>("");
   const { showOverlay, hideOverlay } = useContext(OverlayContext);
   const showDeleteConfirmation = () => {
     setShowDelete(true);
@@ -50,9 +53,16 @@ const DatasetCategories: NextPageLayout<Props> = ({ datasetCategoriesProps }: Pr
 
   const handleCreateCategory = async (): Promise<void> => {
     if (categoryName.length >= 3) {
-      const newDataCategory = await createDataCategory(categoryName);
-      setDatasetCategories([...datasetCategories, newDataCategory.data]);
-      setCategoryName("");
+      try {
+        const newDataCategory = await createDataCategory(categoryName);
+        setDatasetCategories([...datasetCategories, newDataCategory.data]);
+        setCategoryName("");
+        setCreationError("");
+      } catch (error) {
+        const errorWrapper = error as AxiosError;
+        const errorData = errorWrapper.response!.data as IValidationError;
+        setCreationError(errorData.errors.Name![0]);        
+      }
     }
   };
   const handleCategoryChange = (e: FormEvent<HTMLInputElement>) => setCategoryName(e.currentTarget.value);
@@ -62,10 +72,14 @@ const DatasetCategories: NextPageLayout<Props> = ({ datasetCategoriesProps }: Pr
     setDatasetCategories(newDatasetCategories);
   };
 
+  const baseTitleStyle =
+    "border-b-2 border-gray-300 rounded-t block w-4/5 mx-auto py-2 pl-3 text-center focus:outline-none";
+  const errorTitleStyle = baseTitleStyle + " text-red-600 bg-red-100 border-red-400 animate-pulse";
+
   return (
     <div className='mx-auto w-11/12 md:w-4/6 lg:w-3/5 xl:w-1/2'>
       <TopElement text='Manage datasets and data categories' />
-      <div className='flex items-center text-lg w-5/6 mx-auto rounded-md mt-10 mb-16'>
+      <div className='flex items-center text-lg mx-auto rounded-md mt-10 mb-16'>
         <div className='w-min mx-auto cursor-pointer group'>
           <PlusCircle
             size={36}
@@ -78,7 +92,7 @@ const DatasetCategories: NextPageLayout<Props> = ({ datasetCategoriesProps }: Pr
         </div>
         <input
           type='text'
-          className='border-b-2 border-gray-300 rounded-t block w-4/5 mx-auto py-2 pl-3 text-center focus:outline-none'
+          className={creationError.length === 0 ? baseTitleStyle : errorTitleStyle}
           aria-label='input-name'
           placeholder='Enter data category name'
           onChange={handleCategoryChange}
@@ -93,10 +107,7 @@ const DatasetCategories: NextPageLayout<Props> = ({ datasetCategoriesProps }: Pr
           .sort((a, b) => (a.id > b.id ? -1 : 1))
           .map((dataCategory) => {
             return (
-              <div
-                className='mx-auto border py-2 my-10 shadow-md rounded bg-white'
-                key={dataCategory.id}
-              >
+              <div className='mx-auto border py-2 my-10 shadow-md rounded bg-white' key={dataCategory.id}>
                 <DatasetCategoryElement
                   dataCategory={dataCategory}
                   setCurrentDataCategoryId={setCurrentDataCategoryId}
