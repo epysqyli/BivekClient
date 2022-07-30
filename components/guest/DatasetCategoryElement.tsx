1a57decd556082f1d3a50ed68b8528e4ab2b5735
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-30 14:55:18 +0200

    guest: improve style

--- components/guest/DatasetCategoryElement.tsx
+++ components/guest/DatasetCategoryElement.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import { Droplet } from "react-feather";
 import IDataCategory from "../../interfaces/models/IDataCategory";
 import DatasetElement from "./DatasetElement";
 
@@ -9,7 +10,7 @@ interface Props {
 const DatasetCategoryElement = ({ datasetCategory }: Props): ReactElement => {
   return (
     <div className='my-10 py-5 px-2 mx-auto'>
-      <h2 className="text-2xl text-center mb-12 font-medium text-slate-700">{datasetCategory.name}</h2>
+      <h2 className="text-3xl tracking-tighter text-center mb-12 font-medium text-slate-700">{datasetCategory.name}</h2>
       <div>
         {datasetCategory.datasets.map((dataset) => (
           <DatasetElement dataset={dataset} key={dataset.id} />
--- components/guest/WorkingPaperElement.tsx
+++ components/guest/WorkingPaperElement.tsx
@@ -10,7 +10,9 @@ interface Props {
 const WorkingPaperElement = ({ workingPaper }: Props): ReactElement => {
   return (
     <div className='my-10 py-6 px-2 mx-auto border-b-2 hover:bg-slate-200 active:bg-slate-300 hover:rounded hover:border-transparent'>
-      <h2 className='text-2xl text-center mb-5 font-medium text-slate-700'>{workingPaper.title}</h2>
+      <h2 className='text-3xl tracking-tighter text-center mb-5 font-medium text-slate-700'>
+        {workingPaper.title}
+      </h2>
       <div className='text-xl text-center mb-10 text-slate-600'>{workingPaper.createdAt}</div>
       <div className='text-slate-700 w-5/6 mx-auto text-justify'>{workingPaper.abstract}</div>
       <div className='my-12 w-5/6 mx-auto'>
@@ -20,7 +22,7 @@ const WorkingPaperElement = ({ workingPaper }: Props): ReactElement => {
             <Download
               size={28}
               strokeWidth={1.5}
-              className='transition-transform group-hover:scale-95 group-active:scale-90'
+              className='transition-transform group-hover:scale-95 group-active:scale-90 text-amber-600'
             />
           </a>
         </Link>
@@ -29,8 +31,9 @@ const WorkingPaperElement = ({ workingPaper }: Props): ReactElement => {
             <a className='flex items-center justify-between cursor-pointer my-5 py-2 px-2 rounded text-slate-600 group hover:bg-slate-300'>
               <div>Download dataset</div>
               <Download
+                size={28}
                 strokeWidth={1.5}
-                className='transition-transform group-hover:scale-95 group-active:scale-90'
+                className='transition-transform group-hover:scale-95 group-active:scale-90 text-amber-600'
               />
             </a>
           </Link>
