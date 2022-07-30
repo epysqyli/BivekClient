0e4aef5406620cbbe2d2ddb84a763d666ecc9386
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-30 14:58:14 +0200

    admin: adjust footer floating navbar style

--- layouts/AdminLayout.tsx
+++ layouts/AdminLayout.tsx
@@ -23,27 +23,27 @@ const AdminLayout = ({ children }: ILayoutProps): ReactElement => {
     <OverlayProvider value={{ showOverlay, hideOverlay }}>
       <header></header>
       <main className='pb-24'>{children}</main>
-      <footer className='fixed bottom-0 w-full lg:w-1/2 xl:w-1/3 lg:fixed lg:left-1/2 lg:-translate-x-1/2 lg:bottom-10 lg:shadow lg:rounded-md lg:hover:shadow-md lg:opacity-50 lg:hover:opacity-100 transition-opacity bg-slate-100 py-3 lg:py-5'>
+      <footer className='fixed bottom-0 w-full lg:w-2/5 xl:w-1/3 lg:fixed lg:left-1/2 lg:-translate-x-1/2 lg:bottom-10 lg:shadow lg:rounded-md lg:hover:shadow-md lg:opacity-50 lg:hover:opacity-100 lg:bg-neutral-50 transition-opacity py-3 lg:py-5 lg:border-2 lg:border-amber-500'>
         <div className='flex justify-around items-center w-4/5 mx-auto'>
           <Link href='/'>
             <Globe
               size={26}
               strokeWidth={1.5}
-              className='cursor-pointer text-slate-500 hover:text-slate-700 transition-transform hover:scale-95 active:scale-90'
+              className='cursor-pointer text-amber-600 hover:text-slate-800 transition-transform hover:scale-95 active:scale-90'
             />
           </Link>
           <Link href='/admin'>
             <Settings
               size={26}
               strokeWidth={1.5}
-              className='cursor-pointer text-slate-500 hover:text-slate-700 transition-transform hover:scale-95 active:scale-90'
+              className='cursor-pointer text-amber-600 hover:text-slate-800 transition-transform hover:scale-95 active:scale-90'
             />
           </Link>
           <LogOut
             onClick={handleLogout}
             size={26}
             strokeWidth={1.5}
-            className='cursor-pointer text-slate-500 hover:text-slate-700 transition-transform hover:scale-95 active:scale-90'
+            className='cursor-pointer text-amber-600 hover:text-slate-800 transition-transform hover:scale-95 active:scale-90'
           />
         </div>
       </footer>
