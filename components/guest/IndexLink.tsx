42837d310a664981d55415c4f6511f386f222c6a
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-08-01 15:45:28 +0200

    index: improve navbar items positioning

--- components/guest/IndexLink.tsx
+++ components/guest/IndexLink.tsx
@@ -10,7 +10,7 @@ interface Props {
 const IndexLink = ({ pageLink, item, icon }: Props): ReactElement => {
   return (
     <Link href={pageLink}>
-      <div className='p-3 w-1/4 md:w-1/5 cursor-pointer group text-slate-100 border-2 rounded-md bg-slate-600 hover:text-amber-500 transition-colors shadow-md shadow-slate-400 dark:shadow-none hover:border-amber-500'>
+      <div className='py-3 w-1/5 cursor-pointer group text-slate-100 border-2 rounded-md bg-slate-600 hover:text-amber-500 transition-colors shadow-md shadow-slate-400 dark:shadow-none hover:border-amber-500'>
         {icon}
         <span className='block text mt-2 lg:mt-4 text-center'>{item}</span>
       </div>
