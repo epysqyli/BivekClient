4939414484ca8ee3defbdc619e441b29d92b02d1
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-08-01 15:34:58 +0200

    dark mode: adjust research papers

--- components/guest/WorkingPaperElement.tsx
+++ components/guest/WorkingPaperElement.tsx
@@ -9,15 +9,19 @@ interface Props {
 
 const WorkingPaperElement = ({ workingPaper }: Props): ReactElement => {
   return (
-    <div className='my-10 py-6 px-2 mx-auto border-b border-slate-400 hover:bg-slate-300 hover:rounded hover:border-transparent'>
-      <h2 className='text-3xl tracking-tighter text-center mb-5 font-medium text-slate-700'>
+    <div className='my-10 py-6 px-2 mx-auto border-b border-slate-400'>
+      <h2 className='text-3xl tracking-tighter text-center mb-5 font-medium text-slate-700 dark:text-slate-100'>
         {workingPaper.title}
       </h2>
-      <div className='text-xl text-center mb-10 text-slate-600'>{workingPaper.createdAt}</div>
-      <div className='text-slate-700 w-5/6 mx-auto text-justify'>{workingPaper.abstract}</div>
+      <div className='text-xl text-center mb-10 text-slate-600 dark:text-slate-400'>
+        {workingPaper.createdAt}
+      </div>
+      <div className='text-slate-700 dark:text-slate-200 w-5/6 mx-auto text-justify'>
+        {workingPaper.abstract}
+      </div>
       <div className='my-12 w-5/6 mx-auto'>
         <Link href={workingPaper.link}>
-          <a className='flex items-center justify-between cursor-pointer my-5 py-2 px-2 rounded text-slate-600 group hover:bg-slate-100 active:bg-slate-200'>
+          <a className='flex items-center justify-between cursor-pointer my-5 py-2 px-2 rounded text-slate-600 dark:text-slate-200 group hover:bg-slate-100 active:bg-slate-200 dark:hover:bg-slate-600 dark:active:bg-slate-700'>
             <div>Download paper</div>
             <Download
               size={28}
@@ -28,7 +32,7 @@ const WorkingPaperElement = ({ workingPaper }: Props): ReactElement => {
         </Link>
         {workingPaper.datasetLink ? (
           <Link href={workingPaper.link}>
-            <a className='flex items-center justify-between cursor-pointer my-5 py-2 px-2 rounded text-slate-600 group hover:bg-slate-100 active:bg-slate-200'>
+            <a className='flex items-center justify-between cursor-pointer my-5 py-2 px-2 rounded text-slate-600 dark:text-slate-200 group hover:bg-slate-100 active:bg-slate-200 dark:hover:bg-slate-600 dark:active:bg-slate-700'>
               <div>Download dataset</div>
               <Download
                 size={28}
