77c5799b2c82966b13167cb0881606618573c360
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-08-01 15:29:58 +0200

    dark mode: adjust datasets

--- components/guest/DatasetCategoryElement.tsx
+++ components/guest/DatasetCategoryElement.tsx
@@ -9,8 +9,8 @@ interface Props {
 
 const DatasetCategoryElement = ({ datasetCategory }: Props): ReactElement => {
   return (
-    <div className='my-10 pt-5 pb-10 px-2 mx-auto border-b border-slate-400'>
-      <h2 className="text-3xl tracking-tighter text-center mb-12 font-medium text-slate-700">{datasetCategory.name}</h2>
+    <div className='my-10 pt-5 pb-10 px-2 mx-auto border-b border-slate-400 dark:border-none'>
+      <h2 className="text-3xl tracking-tighter text-center mb-12 font-medium text-slate-700 dark:text-slate-100">{datasetCategory.name}</h2>
       <div>
         {datasetCategory.datasets.map((dataset) => (
           <DatasetElement dataset={dataset} key={dataset.id} />
--- components/guest/DatasetElement.tsx
+++ components/guest/DatasetElement.tsx
@@ -10,8 +10,8 @@ interface Props {
 const DatasetElement = ({ dataset }: Props): ReactElement => {
   return (
     <Link href={dataset.link}>
-      <div className='mx-auto px-2 py-3 flex justify-between border-b hover:bg-slate-300 active:bg-slate-400 hover:rounded cursor-pointer'>
-        <div className='w-4/5 text-slate-700'>{dataset.title}</div>
+      <div className='mx-auto px-2 py-3 flex justify-between border-b dark:border-slate-600 hover:bg-slate-300 dark:hover:bg-slate-600 dark:active:bg-slate-700 active:bg-slate-400 hover:rounded cursor-pointer'>
+        <div className='w-4/5 text-slate-700 dark:text-slate-100'>{dataset.title}</div>
         <Download className='w-1/5 text-amber-600 transition-transform hover:scale-95 active:scale-90' />
       </div>
     </Link>
