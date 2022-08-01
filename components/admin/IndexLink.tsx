eac4ce968751b99b1fcaacb903702c95bfb39070
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-08-01 17:05:50 +0200

    dark mode: fix admin index colors

--- components/admin/IndexLink.tsx
+++ components/admin/IndexLink.tsx
@@ -11,7 +11,7 @@ const IndexLink = ({ text, href, icon }: Props): ReactElement => {
   return (
     <>
       <Link href={href}>
-        <div className='border-2 border-amber-500 bg-slate-500 shadow-md shadow-slate-300 hover:shadow-slate-400 rounded-md hover:border-amber-700 cursor-pointer transition-all group flex items-center justify-between px-10 py-6'>
+        <div className='border-2 border-amber-500 bg-slate-500 shadow-md shadow-slate-300 dark:shadow-none hover:shadow-slate-400 rounded-md hover:border-amber-700 cursor-pointer transition-all group flex items-center justify-between px-10 py-6'>
           <div>{icon}</div>
           <div className='text-lg tracking-tight text-slate-50 group-hover:text-white text-right'>{text}</div>
         </div>
