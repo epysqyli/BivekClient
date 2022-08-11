36d4032b9e8b8cec23c79a9b2c4bbacb766f5f4e
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-08-11 19:00:00 +0200

    dark mode: simplify dark mode toggle component

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -4,24 +4,28 @@ import { DarkModeContext } from "../hooks/DarkModeContext";
 
 const DarkModeToggle = (): ReactElement => {
   const { toggleDarkMode, isDarkMode } = useContext(DarkModeContext);
+  const lightStyle = "py-2 lg:py-5 mx-auto border-b-2 border-amber-400 cursor-pointer";
+  const darkStyle =
+    "py-2 lg:py-5 mx-auto border-b-2 border-neutral-400 hover:border-neutral-200 group cursor-pointer";
+
+  const sunIcon: ReactElement = (
+    <Sun size={36} strokeWidth={1.5} fill='#fbbf24' className='w-min mx-auto text-amber-400' />
+  );
+
+  const moonIcon: ReactElement = (
+    <Moon
+      size={36}
+      strokeWidth={1.5}
+      fill='#737373'
+      className='w-min mx-auto text-neutral-400 group-hover:text-neutral-200'
+    />
+  );
 
   return (
     <>
-      {isDarkMode() ? (
-        <div
-          onClick={() => toggleDarkMode()}
-          className='py-2 lg:py-5 mx-auto border-b-2 border-amber-400 cursor-pointer'
-        >
-          <Sun size={36} strokeWidth={1.5} fill='#fbbf24' className='w-min mx-auto text-amber-400' />
-        </div>
-      ) : (
-        <div
-          onClick={() => toggleDarkMode()}
-          className='py-2 lg:py-5 mx-auto border-b-2 border-neutral-400 hover:border-neutral-200 group cursor-pointer'
-        >
-          <Moon size={36} strokeWidth={1.5} fill='#737373' className='w-min mx-auto text-neutral-400 group-hover:text-neutral-200' />
-        </div>
-      )}
+      <div onClick={() => toggleDarkMode()} className={isDarkMode() ? darkStyle : lightStyle}>
+        {isDarkMode() ? sunIcon : moonIcon}
+      </div>
     </>
   );
 };
