34cd10b93b84db8bc8a47693ee1fa9ece838b833
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-08-13 18:15:15 +0200

    index: add asdasdasd.

--- pages/index.tsx
+++ pages/index.tsx
@@ -101,3 +101,6 @@ const Home: NextPageLayout<Props> = ({ latestArticle, tags }: Props): ReactEleme
 Home.getLayout = (page: ReactElement) => <GuestLayout>{page}</GuestLayout>;
 
 export default Home;
+
+// asdasdasdasdasd
+// more asdasdasda
