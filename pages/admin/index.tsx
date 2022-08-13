c05a8d5e6d400bba08a190da2333ceb15a6827e0
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-08-13 15:05:28 +0200

    admin: add random comment on index.

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -73,3 +73,5 @@ const AdminIndex: NextPageLayout = (): ReactElement => {
 AdminIndex.getLayout = (page: ReactElement) => <AdminLayout>{page}</AdminLayout>;
 
 export default AdminIndex;
+
+// adding some random comments on admin index
