7373100cd5479b6c39c80126c7f59e1de433a5fa
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-05-12 21:59:19 +0200

    about: add desc.

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,12 +6,37 @@ import Head from "next/head";
 
 const About: NextPageLayout = (): ReactElement => {
   return (
-    <div className="lg:w-3/4 xl:w-3/5 2xl:w-1/2 mx-auto">
+    <div className='lg:w-3/4 xl:w-3/5 2xl:w-1/2 mx-auto'>
       <Head>
         <title>About</title>
       </Head>
       <SectionHeader resource='about' text='About' />
-      <div className='mt-10'></div>
+      <div className='my-16 text-white mx-auto w-4/5 text-justify'>
+        <p className="my-5">
+          Hi y'all. I am Bivek. When I had the idea of writing a blog, the Chatgpt was not around. So, I
+          thought it would be a good idea to write about things I am interested in and if the luck strikes,
+          some people might even read it. I guess I now have to revise my forecast about the latter part.
+        </p>
+
+        <p className="my-5">
+          Although portfolio constructions and present value analysis are my flat mates, I like to think that
+          I might have a bit more room for couple of other things in my life. When I am not discussing how
+          much efficiency is there in a certain market with my mates (Yes, you heard it right! The finance
+          nerds do talk about these things even when they are in a bar), I like to read and discuss philosophy
+          (particularly the branch of political philosophy), Physics (or more so science in general),
+          everything Data, Mathematics etc. Besides that, I am a beginner night sky watcher (Beginner
+          Astronomer sounds more fancy but that's too heavy title for me right now). Taking my telescope to
+          the suburbs and watching the rings of Saturn is beyond satisfaction.
+        </p>
+        <p className="my-5">
+          I am also a guitar player who has learned enough to noodle up and down the fretboard without making
+          any progress musically for the last 10 years. I guess that's enough of an introduction for now. My
+          goals with this blog is to try to improve my own understanding of the things I know and more
+          importantly, things I like to know about. I often hear writing is a good way of expressing your own
+          opinions and perspective to yourself.
+        </p>
+        <p className="my-5">Let's see if I am able to make the future me laugh.</p>
+      </div>
     </div>
   );
 };
